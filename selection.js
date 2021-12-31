55bc18b9b9326356cfd1a7251d5038a1b74f4f5c
Virginia Wu <virginiawu11@gmail.com>
2021-12-30 21:47:24 -0500

    complete selection

--- selection.js
+++ selection.js
@@ -1,3 +1,18 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let currentMin = arr[i];
+    let currentMinIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < currentMin) {
+        currentMin = arr[j];
+        currentMinIndex = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = currentMin;
+    arr[currentMinIndex] = temp;
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
