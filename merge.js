6aa98279f75182ef645e6a787138b31280bb6b56
Virginia Wu <virginiawu11@gmail.com>
2021-12-30 21:47:33 -0500

    complete merge function

--- merge.js
+++ merge.js
@@ -1,5 +1,27 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let leftP = 0;
+  let rightP = 0;
+  let mergedArr = [];
+  while (leftP < arr1.length && rightP < arr2.length) {
+    if (arr1[leftP] <= arr2[rightP]) {
+      mergedArr.push(arr1[leftP]);
+      leftP++;
+    } else {
+      mergedArr.push(arr2[rightP]);
+      rightP++;
+    }
+  }
+  while (leftP < arr1.length) {
+    mergedArr.push(arr1[leftP]);
+    leftP++;
+  }
+  while (rightP < arr2.length) {
+    mergedArr.push(arr2[rightP]);
+    rightP++;
+  }
+  return mergedArr;
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
