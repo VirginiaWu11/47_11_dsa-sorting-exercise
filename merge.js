e3c17096d103bb6e2b87a2ea42f7c57ea125c009
Virginia Wu <virginiawu11@gmail.com>
2021-12-30 23:24:28 -0500

    complete mergeSort

--- merge.js
+++ merge.js
@@ -22,6 +22,28 @@ function merge(arr1, arr2) {
   return mergedArr;
 }
 
-function mergeSort() {}
+function mergeSort(arr) {
+  // base case
+  if (arr.length <= 1) return arr;
+  const midIndex = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, midIndex));
+  const right = mergeSort(arr.slice(midIndex));
+  console.log(merge(left, right));
+  return merge(left, right);
+}
 
 module.exports = { merge, mergeSort };
+
+//  0   1  2   3   4   5  6  7
+// [4, 20, 12, *10, 7, 9, 1]
+
+// mid = 3
+// left <                          right <
+// 4, 20, 12                   10, 7, 9, 1
+// L     R               L                 R
+// 4    20, 12        10, 7               9, 1
+//     L   R          L   R               L   R
+//     20  12        10    7              9   1
+//     [12,20]        [7,10]           [1,9]
+// [4,12,20]<                  [1,7,9,10]<
+//             [1,4,7,9,10,12,20] DONE
